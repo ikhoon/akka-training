18fed383906d3a126e5dbe102b64d879bac5e762
Ikhoon Eom <ih.pert@gmail.com>
2016-07-18 23:52:27 +0900

    Crash child Actor

--- src/test/scala/fault/tolerance/SupervisorSpec.scala
+++ src/test/scala/fault/tolerance/SupervisorSpec.scala
@@ -1,9 +1,10 @@
 package fault.tolerance
 
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props, Terminated}
 import akka.testkit.{ImplicitSender, TestKit}
 import com.typesafe.config.ConfigFactory
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec, WordSpecLike}
+import scala.concurrent.duration._
 
 /**
   * Created by ikhoon on 2016. 7. 17..
@@ -40,9 +41,20 @@ class SupervisorSpec(_system: ActorSystem) extends TestKit(_system)
       child ! "get"
       expectMsg(42)
 
-      child ! new NullPointerException // crash
+      child ! new NullPointerException // crash & restart
       child ! "get"
       expectMsg(0)
+
+      watch(child) // testActor watch child
+      child ! new IllegalArgumentException // crash & stop
+      expectMsgPF() {
+        case Terminated(`child`) => ()
+      }
+      child ! "get" // 대답 없음
+      expectNoMsg(3 second)
+
+      child ! "get" // 역시나 대답없음
+      expectNoMsg(5 second)
     }
   }
 
